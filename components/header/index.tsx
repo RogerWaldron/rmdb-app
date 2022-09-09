aaec6411bd963a3f033c6e67077eeb86246f5820
Roger Waldron <roger.waldron@gmail.com>
2022-09-09 23:55:19 +0200

    feat: :sparkles: Added Header component

---
+++ components/header/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => (
+  <div className="sticky flex w-full h-20 z-999 bg-zinc-900">
+    <div className="flex justify-between w-full h-full px-4 max-w-7xl ">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer">
+          <Image
+            height="64px"
+            width="64px"
+            src="/rmdb-64.png"
+            alt="rmdb logo"
+          />
+        </div>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
