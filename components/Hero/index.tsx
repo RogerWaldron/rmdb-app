3283b9999966a70f64f70aec028a3f41f5655f9c
Roger Waldron <roger.waldron@gmail.com>
2022-09-11 23:56:37 +0200

    refactor: :construction: Stub out components for Card, Grid, Hero, Spinner

---
+++ components/Hero/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  text: string;
+};
+
+const Hero = ({ imgUrl, title, text }: Props) => {
+  <div className="relative w-full h-128">
+    <div className="relative flex flex-col-reverse h-full m-auto max-w-7xl">
+      <div className="max-w-2xl px-4 text-white">
+        <h2 className="pb-8 text-2xl font-bold md:text-5xl">{title}</h2>
+        <p className="text-lg md:text-xl">{text}</p>
+      </div>
+    </div>
+    <Image
+      priority
+      objectFit="cover"
+      objectPosition="center"
+      alt="hero image"
+      src={imgUrl}
+    />
+  </div>;
+};
+
+export default Hero;
