da7e0d4952b48c26712b1b92fc9efa2763b4ab24
Roger Waldron <roger.waldron@gmail.com>
2022-09-12 23:55:15 +0200

    refactor: Created types file for TMDB API

---
+++ api/types.ts
@@ -0,0 +1,27 @@
+export type Movie = {
+  id: number;
+  title: string;
+  status: string;
+  overview: string;
+  release_date: string;
+  original_language: string;
+  runtime: number;
+  budget: number;
+  revenue: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  backdrop_path: string;
+  poster_path: string;
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type Movies = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
