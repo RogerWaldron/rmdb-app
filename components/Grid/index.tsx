73bdb3b1819b984f2be4e57d2ab6ff5764b3c711
Roger Waldron <roger.waldron@gmail.com>
2022-09-14 00:52:51 +0200

    feat: :sparkles: ADD functionality to Grid component stub

--- components/Grid/index.tsx
+++ components/Grid/index.tsx
@@ -1,3 +1,18 @@
-const Grid = () => <div>Grid</div>;
+import React from "react";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Grid = ({ title, children, className }: Props) => (
+  <div className={className}>
+    <h2 className="pb-4 text-xl font-bold">{title}</h2>
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+      {children}
+    </div>
+  </div>
+);
 
 export default Grid;
