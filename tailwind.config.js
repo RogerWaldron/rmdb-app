b29a1b2fe840f006d13c6a04373851f5bb8bed35
Roger Waldron <roger.waldron@gmail.com>
2022-09-13 23:21:57 +0200

    chore: :lipstick: ADD custom h-128 height to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        128: "40rem",
+      },
+    },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
-    }
+    },
   },
   plugins: [],
 };
