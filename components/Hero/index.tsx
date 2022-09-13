d77b2355fc63104c02dbb8292db33f4ac657fbed
Roger Waldron <roger.waldron@gmail.com>
2022-09-13 23:40:03 +0200

    style: :lipstick: ADD style to reduce background image brightness by 0.5 to improve text contrast

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Hero = ({ imgUrl, title, text }: Props) => (
   <div className="relative w-full h-128">
-    <div className="relative flex flex-col-reverse h-full m-auto max-w-7xl">
+    <div className="relative z-10 flex flex-col-reverse h-full pb-12 m-auto text-center max-w-7xl md:text-left">
       <div className="max-w-2xl px-4 text-white">
         <h2 className="pb-8 text-2xl font-bold md:text-5xl">{title}</h2>
         <p className="text-lg md:text-xl">{text}</p>
@@ -21,6 +21,7 @@ const Hero = ({ imgUrl, title, text }: Props) => (
       alt="hero image"
       layout="fill"
       src={imgUrl}
+      style={{ filter: "brightness(0.5)" }}
     />
   </div>
 );
