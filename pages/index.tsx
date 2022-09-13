c1f68ef065d74fde37d3ae3df4e7f6b9a4ded87b
Roger Waldron <roger.waldron@gmail.com>
2022-09-14 00:53:56 +0200

    feat: :sparkles: ADD Grid component shows movie Card components

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Grid from "@/components/Grid";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Spinner from "@/components/Spinner";
-import { BACKDROP_SIZE, IMAGE_BASE_URL } from "@/api_tmdb/config";
+import { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE } from "@/api_tmdb/config";
 
 const Home: NextPage = () => {
   const [query, setQuery] = useState("");
@@ -27,12 +27,36 @@ const Home: NextPage = () => {
                 data.pages[0].results[0].backdrop_path
               : ""
           }
-          title={data.pages[0].results[0]?.title}
+          title={data.pages[0].results[0]?.original_title}
           text={data.pages[0].results[0]?.overview}
         />
       ) : null}
-      <Grid />
-      <Card />
+      <Grid
+        className="p-4 m-auto max-w-7xl"
+        title={
+          query
+            ? `Search results: ${data?.pages[0].total_results}`
+            : "Popular Movies"
+        }
+      >
+        {data && data.pages
+          ? data.pages.map((page) =>
+              page.results.map((movie) => (
+                <div key={movie.id}>
+                  <Card
+                    imgUrl={
+                      movie.poster_path
+                        ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                        : ""
+                    }
+                    title={movie.original_title}
+                    subtitle={movie.overview}
+                  />
+                </div>
+              ))
+            )
+          : null}
+      </Grid>
       <Spinner />
     </main>
   );
