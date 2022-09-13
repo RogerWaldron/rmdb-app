aa5aabcc7a62bcce02455e4ffc913f50a81d1560
Roger Waldron <roger.waldron@gmail.com>
2022-09-14 00:51:55 +0200

    feat: :sparkles: ADD Card component uses Thumbnail component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,3 +1,25 @@
-const Card = () => <div>Card</div>;
+import Thumbnail from "@/components/Thumbnail";
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  subtitle?: string;
+};
+
+const Card = ({ imgUrl, title, subtitle }: Props) => (
+  <div className="h-80">
+    <div className="relative h-full">
+      <Thumbnail imgUrl={imgUrl} />
+      <div className="absolute bottom-0 w-full px-4 py-2 rounded-b-xl bg-zinc-800">
+        <h2 className="text-sm font-bold text-gray-300 truncate">{title}</h2>
+        {subtitle ? (
+          <p className="text-xs text-center text-gray-400 truncate">
+            {subtitle}
+          </p>
+        ) : null}
+      </div>
+    </div>
+  </div>
+);
 
 export default Card;
